0ab44a273629feb8b57bf091407f1c450bb5b479
MrNacatamal09 <acramireza@uamv.edu.ni>
2025-10-15 19:21:31 -0600

    Agregados tres programas en Python por Adolfo

---
+++ promedio.py
@@ -0,0 +1,9 @@
+print("Bienvenido/a al sistema de Promedio")
+a = float(input("Ingrese la primera nota: "))
+b = float(input("Ingrese la segunda nota: "))
+c = float(input("Ingrese la tercera nota: "))
+
+promedio = (a + b + c) / 3
+
+print("------------------------------")
+print(f"El promedio de las notas es: {promedio:.2f}")
---
+++ suma.py
@@ -0,0 +1,7 @@
+print("Bienvenido/a al sistema Suma")
+a = int(input("Ingrese el primer digito: "))
+print("------------------------------")
+b = int(input("Ingrese el segundo digito: "))
+resultado = a + b
+print("------------------------------")
+print(f"El resultado de {a} + {b} es {resultado}")
